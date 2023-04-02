2a6405fc43c8e78a25380161597eac6aa5f7e52f
Eric-Barajas <barajaseric505@gmail.com>
2023-04-02 00:47:28 -0700

    init commit

--- cookie_clicker/script.js
+++ cookie_clicker/script.js
@@ -1,3 +1,14 @@
-function noItems() {
-    alert("Your Cart is empty!");
-}
+
+
+var object = document.getElementById('bounce');
+var count = 0
+
+object.onclick = function () {
+    count++
+    var element = document.querySelector(".header")
+    element.innerText = `${count} Cookies!`
+    var x = Math.floor(Math.random() * 30);
+    var y = Math.floor(Math.random() * 30);
+    object.style.top = x + 'px';
+    object.style.left = y + 'px';
+};
