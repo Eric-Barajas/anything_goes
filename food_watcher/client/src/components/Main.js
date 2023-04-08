4a5f571cf6e72332938f9452702abf36440bcd7f
Eric-Barajas <barajaseric505@gmail.com>
2023-04-07 22:23:25 -0700

    git init

--- food_watcher/client/src/components/Main.js
+++ food_watcher/client/src/components/Main.js
@@ -1,44 +1,54 @@
 import React from 'react'
 import axios from 'axios'
 import { useState } from 'react'
+import './styling.css';
 
 const Main = () => {
 
-    const [words, setWords] = useState(null)
+    const [ingredient, setIngredient] = useState(null)
+    const [foodinfo, setFoodInfo] = useState(null)
 
     const handleSubmit = (e) => {
         console.log("test")
         e.preventDefault()
         const options = {
             method: 'GET',
-            url: 'https://twinword-word-association-quiz.p.rapidapi.com/type1/',
+            url: 'https://edamam-food-and-grocery-database.p.rapidapi.com/api/food-database/v2/parser',
+            params: {
+                app_key: ' 32cdc5278ce72511dc88d514e264a929	',
+                app_id: '9c77cb81',
+                'nutrition-type': 'cooking',
+                'health[0]': 'alcohol-free',
+                ingr: ingredient,
+                'category[0]': 'generic-foods'
+            },
             headers: {
                 'X-RapidAPI-Key': '3349990788msh388664143710c09p138db8jsn1f85a7af5681',
-                'X-RapidAPI-Host': 'twinword-word-association-quiz.p.rapidapi.com'
+                'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
             }
-        }
+        };
 
-        axios.request(options).then((response) => {
-            console.log(response.data)
-            setWords(response.data)
-        }).catch(err => {
-            console.log(err.response.status)
-            if (err.response.status === 404) {
-                console.log("Mission Failed");
-            }
-        })
+        axios.request(options).then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
     }
 
-    console.log(words && words.quizlist)
+    console.log(foodinfo)
 
     return (
         <div className="app">
             <div>
                 <form className="d-flex" role="search" onSubmit={(e) => handleSubmit(e)}>
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <input className="form-control me-2" value={ingredient} type="search" placeholder="Search" aria-label="Search" />
                     <input className="btn btn-outline-success" type="submit" />
                 </form>
             </div>
+
+            {(ingredient && foodinfo) && <div className="ingr-area">
+
+            </div>}
         </div>
     )
 }
